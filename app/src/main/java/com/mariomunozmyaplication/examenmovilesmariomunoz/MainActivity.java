477538c2fb070356f75a7e60209a86908eaa9a6a
Mario Muñoz Fuentes <mario.munoz.fuentes>
2021-12-10 12:48:51 +0100

    2

--- app/src/main/java/com/mariomunozmyaplication/examenmovilesmariomunoz/MainActivity.java
+++ app/src/main/java/com/mariomunozmyaplication/examenmovilesmariomunoz/MainActivity.java
@@ -1,9 +1,14 @@
 package com.mariomunozmyaplication.examenmovilesmariomunoz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,15 +16,37 @@ public class MainActivity extends AppCompatActivity {
 
     //HACEMOS LAS DECLARACIONES
     private MiAdaptadorSimple adapter;
-    private EditText View;
-    private EditText view;
+    private TextView tv1, tv2, tv3, tv4;
+    private EditText tile;
 
-    //INICIALIZAMOS LOS CONTROLES
+    //ALMACENO EL VALOR DE LAS DIVISAS
+    double USD = 1.1293946;
+    double GBP = 0.85447758;
+    double CAD = 1.4339265;
+    double AUD = 1.5788175;
+    double JPY = 128.17384;
+    double INR = 85.36992;
+    double NZD = 1.6631981;
+    double CHF = 1.0441295;
+    double ZAR = 18.030472;
+    double RUB = 83.219626;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //INICIALIZAMOS LOS CONTROLES
+        //textView
+        tv1= findViewById(R.id.tv1);
+        tv2= findViewById(R.id.tv2);
+        tv3= findViewById(R.id.tv3);
+        tv4= findViewById(R.id.tv4);
+        //editText
+
+
+        //ALMACENO EL NOMBRE DE LAS DIVISAS EN UN ARRAYLIST
         ArrayList<String> divisas= new ArrayList<>();
         divisas.add("USD");     //1,1293946 USD - Dolares estadounidenses
         divisas.add("GBP");     //0,85447758 GBP - Libras esterlinas
@@ -34,21 +61,52 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        RecyclerView recyclerView= findViewById(R.id.rvmd);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayout= new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayout);
 
+        //REFERENCIAMOS CON MiAdaptadorSimple
+        adapter= new MiAdaptadorSimple(this, divisas);
+        recyclerView.setAdapter(adapter);
 
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), mLayout.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
     }
+
+    public void hacerCalculo(View view){
+        switch(view.getId()){
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+
+            case 10:
+                break;
+        }
+    }
+
 }
 
 
-/*
-
-
-
-
-
-
-
-
-
-
- */
--- app/src/main/java/com/mariomunozmyaplication/examenmovilesmariomunoz/MiAdaptadorSimple.java
+++ app/src/main/java/com/mariomunozmyaplication/examenmovilesmariomunoz/MiAdaptadorSimple.java
@@ -1,4 +1,80 @@
 package com.mariomunozmyaplication.examenmovilesmariomunoz;
 
-public class MiAdaptadorSimple {
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MiAdaptadorSimple extends RecyclerView.Adapter<MiAdaptadorSimple.ViewHolder>{
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    private int pos=0;
+    public int getPos(){ return this.pos;}
+    public void decrementarPos(){this.pos--;}
+
+
+    //PASAMOS LOS DATOS AL CONSTRUCTOR
+    MiAdaptadorSimple(Context context, List<String> data){
+        this.mInflater=LayoutInflater.from(context);
+        this.mData=data;
+    }
+
+    //infla cada fila del layout que hemos hecho oara cada fila
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.row,parent,false);
+        return new ViewHolder(view);
+    }
+
+
+    //recupera la posicion del array en el que estamos y
+    //asigna en TExtView el nombre del animal
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //CREAMOS METODO ONCLICK
+            public void onClick(View view) {
+                TextView myTV = view.findViewById(R.id.tvDivisas);
+                ColorDrawable viewColor = (ColorDrawable) myTV.getBackground();
+
+                if (viewColor == null) myTV.setBackgroundColor(Color.BLUE);
+                else {
+                  int colorId = viewColor.getColor();
+                  if (colorId == Color.WHITE) myTV.setBackgroundColor(Color.BLUE);
+                  else myTV.setBackgroundColor(Color.WHITE);
+                }
+                pos=position;
+            }
+        });
+
+    }
+
+    //TOTAL NUMERO DE FILAS
+    @Override
+    public int getItemCount(){
+        return mData.size();
+    }
+
+    //CLASE VIEWHOLDER
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView myTextView;
+        ViewHolder(View itemView){
+            super(itemView);
+            myTextView=itemView.findViewById(R.id.tvDivisas);
+        }
+    }
+
+
 }
